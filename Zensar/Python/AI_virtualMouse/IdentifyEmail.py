9f037039d2652a05d9ba9c9832cc40d13946413a
Kiran Tajanpure <129212680+kiran28092003@users.noreply.github.com>
2025-01-16 07:20:56 +0530

    Delete Zensar/Python/AI_virtualMouse directory

--- Zensar/Python/AI_virtualMouse/IdentifyEmail.py
+++
@@ -1,9 +0,0 @@
-#email
-import re
-Email=input("Enter your email:")
-pattern=r'^[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'
-if re.match(pattern,Email):
-    print("Email pattern matches")
-    print(Email)
-else:
-    print("Invalid email")
--- Zensar/Python/AI_virtualMouse/mobileNumber.py
+++
@@ -1,8 +0,0 @@
-#mobile number
-import re
-Phone=input("Enter your phone number")
-pattern=r'[0-9]'
-if len(Phone)==10 and re.match(pattern,Phone):
-    print("Phone number is valid")
-else:
-    print("Phone number is not valid")
